a931f747b96bc2ae4e9bf4e003c0b0847eb9a9de
Raphael Ramirez <ralph@s2dioapps.com>
2021-08-31 12:29:56 +0800

    Create

--- API/Controllers/TasksController.cs
+++ API/Controllers/TasksController.cs
@@ -7,25 +7,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-  public class TasksController : BaseApiController
-  {
-    [AllowAnonymous]
-    [HttpGet]
-    public async Task<IActionResult> GetTasks()
+    public class TasksController : BaseApiController
     {
-      return HandleResult(await Mediator.Send(new List.Query()));
-    }
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> GetTasks()
+        {
+            return HandleResult(await Mediator.Send(new List.Query()));
+        }
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetTask(Guid id)
-    {
-      return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
-    }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTask(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
+        }
 
-    [HttpPost]
-    public async Task<IActionResult> CreateTask(Domain.Task task)
-    {
-      return HandleResult(await Mediator.Send(new Create.Command { Task = task }));
+        [HttpPost]
+        public async Task<IActionResult> CreateTask(Domain.Task task)
+        {
+            return HandleResult(await Mediator.Send(new Create.Command { Task = task }));
+        }
     }
-  }
 }
--- Application/Tasks/Create.cs
+++ Application/Tasks/Create.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
@@ -9,44 +10,57 @@ using Persistence;
 
 namespace Application.Tasks
 {
-  public class Create
-  {
-    public class Command : IRequest<Result<Unit>>
+    public class Create
     {
-      public Domain.Task Task { get; set; }
-    }
-
-    public class Handler : IRequestHandler<Command, Result<Unit>>
-    {
-      private readonly DataContext _context;
-      private readonly IUsernameAccessor _usernameAccessor;
-      public Handler(DataContext context, IUsernameAccessor usernameAccessor)
-      {
-        _usernameAccessor = usernameAccessor;
-        _context = context;
-      }
-
-      public class CommandValidator : AbstractValidator<Command>
-      {
-        public CommandValidator()
+        public class Command : IRequest<Result<Unit>>
         {
-          RuleFor(x => x.Task).SetValidator(new TaskValidator());
+            public Domain.Task Task { get; set; }
         }
-      }
 
-      public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
-      {
-        // get created by
-        var user = await _context.Users.FirstOrDefaultAsync(user => user.UserName == _usernameAccessor.getUsername());
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _context;
+            private readonly IUsernameAccessor _usernameAccessor;
+            public Handler(DataContext context, IUsernameAccessor usernameAccessor)
+            {
+                _usernameAccessor = usernameAccessor;
+                _context = context;
+            }
 
-        // get assignee
+            public class CommandValidator : AbstractValidator<Command>
+            {
+                public CommandValidator()
+                {
+                    RuleFor(x => x.Task).SetValidator(new TaskValidator());
+                }
+            }
 
-        var result = await _context.SaveChangesAsync() > 0;
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                // get created by
+                var createdBy = await _context.Users.FirstOrDefaultAsync(user => user.UserName == _usernameAccessor.getUsername());
 
-        if (!result) return Result<Unit>.Failure("Failed to create activity");
+                // get assignee
+                var assignee = await _context.Users.FirstOrDefaultAsync(user => user.UserName == request.Task.Assignee.UserName);
 
-        return Result<Unit>.Success(Unit.Value);
-      }
+                if (assignee == null) return null;
+
+                request.Task.CreatedBy = createdBy;
+                request.Task.Assignee = assignee;
+
+                // set DateCreated
+                request.Task.DateCreated = DateTime.Now;
+
+                request.Task.IsCompleted = false;
+
+                _context.Tasks.Add(request.Task);
+
+                var result = await _context.SaveChangesAsync() > 0;
+
+                if (!result) return Result<Unit>.Failure("Failed to create task");
+
+                return Result<Unit>.Success(Unit.Value);
+            }
+        }
     }
-  }
 }
