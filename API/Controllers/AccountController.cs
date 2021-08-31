1bfdd9a4117de6ad9dbd25f3a946fe26384d0c64
Raphael Ramirez <ralph@s2dioapps.com>
2021-08-31 14:13:23 +0800

    Edit

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -10,84 +10,85 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-  [AllowAnonymous]
-  [ApiController]
-  [Route("api/[controller]")]
-  public class AccountController : ControllerBase
-  {
-    private readonly UserManager<AppUser> _userManager;
-    private readonly SignInManager<AppUser> _signInManager;
-    private readonly TokenService _tokenService;
-
-    public AccountController(UserManager<AppUser> userManager,
-        SignInManager<AppUser> signInManager,
-        TokenService tokenService)
+    [AllowAnonymous]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
     {
-      _tokenService = tokenService;
-      _signInManager = signInManager;
-      _userManager = userManager;
-    }
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly TokenService _tokenService;
 
-    [HttpPost("login")]
-    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
-    {
-      var user = await _userManager.FindByNameAsync(loginDto.Username);
-
-      if (user == null) return Unauthorized();
-
-      var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
-
-      if (result.Succeeded)
-      {
-        return new UserDto
+        public AccountController(UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            TokenService tokenService)
         {
-          Username = user.UserName,
-          Token = _tokenService.CreateToken(user),
-          Image = null,
-        };
-      }
+            _tokenService = tokenService;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
 
-      return Unauthorized();
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByNameAsync(loginDto.Username);
+
+            if (user == null) return Unauthorized();
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (result.Succeeded)
+            {
+                return new UserDto
+                {
+                    Username = user.UserName,
+                    Token = _tokenService.CreateToken(user),
+                    Image = null,
+                };
+            }
+
+            return Unauthorized();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
+            {
+                ModelState.AddModelError("username", "Username taken");
+                return ValidationProblem();
+            }
+
+            var user = new AppUser
+            {
+                UserName = registerDto.Username
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (result.Succeeded)
+            {
+                return CreateUserObject(user);
+            }
+
+            return BadRequest("Problem registering user");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await _userManager.FindByNameAsync(User.FindFirstValue(ClaimTypes.Name));
+
+            return CreateUserObject(user);
+        }
+
+        private UserDto CreateUserObject(AppUser user)
+        {
+            return new UserDto
+            {
+                Token = _tokenService.CreateToken(user),
+                Username = user.UserName,
+            };
+        }
     }
-
-    [HttpPost("register")]
-    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
-    {
-      if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
-      {
-        ModelState.AddModelError("username", "Username taken");
-        return ValidationProblem();
-      }
-
-      var user = new AppUser
-      {
-        UserName = registerDto.Username
-      };
-
-      var result = await _userManager.CreateAsync(user, registerDto.Password);
-
-      if (result.Succeeded) {
-        return CreateUserObject(user);
-      }
-
-      return BadRequest("Problem registering user");
-    }
-
-    [HttpGet]
-    public async Task<ActionResult<UserDto>> GetCurrentUser()
-    {
-      var user = await _userManager.FindByNameAsync(User.FindFirstValue(ClaimTypes.Name));
-
-      return CreateUserObject(user);
-    }
-
-    private UserDto CreateUserObject(AppUser user)
-    {
-      return new UserDto
-      {
-        Token = _tokenService.CreateToken(user),
-        Username = user.UserName,
-      };
-    }
-  }
 }
---
+++ Application/Tasks/Edit.cs
@@ -0,0 +1,61 @@
+﻿using Application.Core;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Tasks
+{
+    public class Edit
+    {
+        public class Command : IRequest<Result<Unit>>
+        {
+            public Domain.Task Task { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public class CommandValidator : AbstractValidator<Command>
+            {
+                public CommandValidator()
+                {
+                    RuleFor(x => x.Task).SetValidator(new TaskValidator());
+                }
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var task = await _context.Tasks.FindAsync(request.Task.Id);
+
+                if (task == null) return null;
+
+                // get assignee
+                var assignee = await _context.Users.FirstOrDefaultAsync(user => user.UserName == request.Task.Assignee.UserName);
+                if (assignee == null) return Result<Unit>.Failure("Assignee does not exists.");
+
+                task.Assignee = request.Task.Assignee;
+                task.Name = request.Task.Name;
+                task.Description = request.Task.Description;
+                task.Date = request.Task.Date;
+
+                var result = await _context.SaveChangesAsync() > 0;
+
+                if (!result) return Result<Unit>.Failure("Failed to update task.");
+
+                return Result<Unit>.Success(Unit.Value);
+            }
+        }
+    }
+}
