f8e046280384e110f05575ef994727a74a411d2e
Raphael Ramirez <ralph@s2dioapps.com>
2021-09-02 15:06:02 +0800

    Error fixed

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,7 +12,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Collections.Generic;
-using System.Web.Http.Filters;
 
 namespace API
 {
