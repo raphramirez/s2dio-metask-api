53024fd97e28b9655fc2999cfc7e9ab6d280f086
Raphael Ramirez <ralph@s2dioapps.com>
2021-09-02 17:03:36 +0800

    Update validation message

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,10 +4,10 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
-        [Required]
+        [Required(ErrorMessage = "'Username' must not be empty.")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "'Password' must not be empty.")]
         [MinLength(8, ErrorMessage = "Password must contain at least eight characters long")]
         [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must contain uppercase and lowercase letters and a number.")]
         public string Password { get; set; }
