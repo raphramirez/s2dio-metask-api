0fd4511132d892163723689c8ec1f4feea1d0918
Raphael Ramirez <ralph@s2dioapps.com>
2021-09-09 16:26:36 +0800

    Regex Fixed
    WIP

--- API/DTOs/ChangePasswordDto.cs
+++ API/DTOs/ChangePasswordDto.cs
@@ -13,7 +13,7 @@ namespace API.DTOs
 
         [Required(ErrorMessage = "'New Password' must not be empty.")]
         [MinLength(8, ErrorMessage = "Password must contain at least eight characters long")]
-        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$", ErrorMessage = "Password must contain uppercase and lowercase letters and a number.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "Password must contain uppercase and lowercase letters and a number.")]
         public string NewPassword { get; set; }
     }
 }
