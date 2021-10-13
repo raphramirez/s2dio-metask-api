126629c04c603836638d97e8bb99d3b780f3fd55
Raphael Ramirez <ralph@s2dioapps.com>
2021-10-13 17:04:00 +0800

    Updated Dto

--- Application/Tasks/CreateTaskDto.cs
+++ Application/Tasks/CreateTaskDto.cs
@@ -6,18 +6,13 @@ using System.Threading.Tasks;
 
 namespace Application.Tasks
 {
-    public class CreateTaskDto
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string OrganizationId { get; set; }
-        public DateTime Date { get; set; }
-        public List<AssigneeDto> Assignees { get; set; } = new List<AssigneeDto>();
-    }
-
-    public class AssigneeDto
-    {
-        public string Id { get; set; }
-    }
+  public class CreateTaskDto
+  {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string OrganizationId { get; set; }
+    public DateTime Date { get; set; }
+    public List<string> Assignees { get; set; } = new List<string>();
+  }
 }
