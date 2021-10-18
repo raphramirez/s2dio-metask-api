f489bc3c757a8dde0b1b96bc57001cd0d1dca02a
Raphael Ramirez <ralph@s2dioapps.com>
2021-10-18 14:04:52 +0800

    Fixed: Empty string assignee id

--- Application/Tasks/Edit.cs
+++ Application/Tasks/Edit.cs
@@ -16,67 +16,67 @@ using System.Threading.Tasks;
 
 namespace Application.Tasks
 {
-  public class Edit
-  {
-    public class Command : IRequest<Result<Unit>>
+    public class Edit
     {
-      public CreateTaskDto Task { get; set; }
-    }
-
-    public class Handler : IRequestHandler<Command, Result<Unit>>
-    {
-      private readonly PlutoContext _context;
-      private readonly ITaskRepository _taskRepository;
-      private readonly IUserRepository _userRepository;
-      private readonly IMapper _mapper;
-      private readonly FirebaseNotificationService _notificationService;
-
-      public Handler(ITaskRepository taskRepository, IUserRepository userRepository, IMapper mapper, FirebaseNotificationService notificationService)
-      {
-        _taskRepository = taskRepository;
-        _userRepository = userRepository;
-        _mapper = mapper;
-        _notificationService = notificationService;
-      }
-
-      public class CommandValidator : AbstractValidator<Command>
-      {
-        public CommandValidator()
+        public class Command : IRequest<Result<Unit>>
         {
-          RuleFor(x => x.Task).SetValidator(new TaskValidator());
+            public CreateTaskDto Task { get; set; }
         }
-      }
 
-      public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
-      {
-        var task = await _taskRepository.FirstOrDefault(x => x.Id == request.Task.Id,
-            // includes
-            x => x.UserTasks,
-            x => x.CreatedBy
-        );
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly PlutoContext _context;
+            private readonly ITaskRepository _taskRepository;
+            private readonly IUserRepository _userRepository;
+            private readonly IMapper _mapper;
+            private readonly FirebaseNotificationService _notificationService;
 
-        if (task == null) return null;
-
-        task.Name = request.Task.Name;
-        task.Description = request.Task.Description;
-        task.Date = request.Task.Date;
-
-        var changes = await _taskRepository.Edit();
-        if (!(changes > 0)) return Result<Unit>.Failure(
-            new ApiErrorResponse
+            public Handler(ITaskRepository taskRepository, IUserRepository userRepository, IMapper mapper, FirebaseNotificationService notificationService)
             {
-              Title = "Request failed.",
-              Instance = "/api/tasks/{id}",
-              Status = (int)HttpStatusCode.BadRequest,
-              Errors = new string[]
-              {
-                "Failed to edit task."
-              }
+                _taskRepository = taskRepository;
+                _userRepository = userRepository;
+                _mapper = mapper;
+                _notificationService = notificationService;
             }
-        );
 
-        return Result<Unit>.Success(Unit.Value);
-      }
+            public class CommandValidator : AbstractValidator<Command>
+            {
+                public CommandValidator()
+                {
+                    RuleFor(x => x.Task).SetValidator(new TaskValidator());
+                }
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var task = await _taskRepository.FirstOrDefault(x => x.Id == request.Task.Id,
+                    // includes
+                    x => x.UserTasks,
+                    x => x.CreatedBy
+                );
+
+                if (task == null) return null;
+
+                task.Name = request.Task.Name;
+                task.Description = request.Task.Description;
+                task.Date = request.Task.Date;
+
+                var changes = await _taskRepository.Edit();
+                if (!(changes > 0)) return Result<Unit>.Failure(
+                    new ApiErrorResponse
+                    {
+                        Title = "Request failed.",
+                        Instance = "/api/tasks/{id}",
+                        Status = (int)HttpStatusCode.BadRequest,
+                        Errors = new string[]
+                      {
+                        "Failed to edit task."
+                      }
+                    }
+                );
+
+                return Result<Unit>.Success(Unit.Value);
+            }
+        }
     }
-  }
 }
