460c24ed273de9d51d7e002f57c1854aace12416
Raphael Ramirez <ralph@s2dioapps.com>
2021-10-18 13:25:18 +0800

    Manage & Invite Organization Access

--- API/Extensions/AuthorizationServiceExtensions.cs
+++ API/Extensions/AuthorizationServiceExtensions.cs
@@ -27,7 +27,7 @@ namespace API.Extensions
                 });
 
                 // setting up policies for tasks
-                options.AddPolicy("ReadAccess", policy => 
+                options.AddPolicy("ReadAccess", policy =>
                     policy.RequireAssertion(context =>
                        context.User.HasClaim(claim =>
                          (claim.Type == "permissions" &&
@@ -38,7 +38,7 @@ namespace API.Extensions
                     )
                   );
 
-                options.AddPolicy("WriteAccess", policy => 
+                options.AddPolicy("WriteAccess", policy =>
                     policy.RequireAssertion(context =>
                        context.User.HasClaim(claim =>
                          (claim.Type == "permissions" &&
@@ -50,11 +50,34 @@ namespace API.Extensions
                     )
                   );
 
-                  options.AddPolicy("DeleteAccess", policy => 
+                options.AddPolicy("DeleteAccess", policy =>
+                  policy.RequireAssertion(context =>
+                     context.User.HasClaim(claim =>
+                       (claim.Type == "permissions" &&
+                        (claim.Value == "delete:tasks") &&
+                        claim.Issuer == $"https://{config["Auth0:Domain"]}/"
+                       )
+                     )
+                  )
+                );
+
+                // Organization
+                options.AddPolicy("InviteAccess", policy =>
                     policy.RequireAssertion(context =>
                        context.User.HasClaim(claim =>
                          (claim.Type == "permissions" &&
-                          (claim.Value == "delete:tasks") &&
+                          (claim.Value == "invite:organization") &&
+                          claim.Issuer == $"https://{config["Auth0:Domain"]}/"
+                         )
+                       )
+                    )
+                  );
+
+                options.AddPolicy("ManageOrganizationAccess", policy =>
+                    policy.RequireAssertion(context =>
+                       context.User.HasClaim(claim =>
+                         (claim.Type == "permissions" &&
+                          (claim.Value == "manage:organization") &&
                           claim.Issuer == $"https://{config["Auth0:Domain"]}/"
                          )
                        )
